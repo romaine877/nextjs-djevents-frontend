068998956776b62beb3507b7fc8e6bf062d48410
romaine877 <romaine877@gmail.com>
2021-12-25 23:02:20 -0500

    jwt accepted and stored in cookie

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -186,11 +186,10 @@ export default function EditEventPage({event}) {
 }
 
 
-export async function getServerSideProps({params: {id}}){
+export async function getServerSideProps({params: {id}, req}){
   
   const res = await fetch(`${API_URL}/events/${id}`)
   const event = await res.json()
-
   return{
     props: {event}
   }
