234696ea244fca3c28c0a1b6bf9cefb2a92cf979
romaine877 <romaine877@gmail.com>
2022-01-02 21:02:05 -0500

    dashboard page created

---
+++ components/DashboardItem.js
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import { FaPencilAlt, FaTimes } from "react-icons/fa"
+import styles from "@/styles/Dashboard.module.css"
+
+export default function DashboardItem({event, handleDelete}) {
+    return (
+        <div className={styles.event}>
+            <Link href={`/events/${event.slug}`}>
+            <h4>{event.name}</h4>
+            </Link>
+            <div>
+               
+
+                
+                <Link href={`/events/edit/${event.id}`}>
+                <a className={styles.edit}>
+                <FaPencilAlt/>
+                <span>Edit</span>
+                </a>
+
+                </Link>
+               
+                <a href="#" className={styles.delete} onClick={()=>handleDelete(event.id)}>
+                <FaTimes/>
+                <span>Delete</span>
+                </a>
+
+                
+
+            </div>
+        </div>
+    )
+}
--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,16 +1,55 @@
-import EventItem from "@/components/EventItem";
+import DashboardItem from "@/components/DashboardItem";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import { getCookie } from "../../helpers";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from "next/router";
 
 export default function DashboardPage({events}) {
+
+    const router = useRouter()
+
+    const deleteItem = async (id) => {
+        
+
+        
+
+            const res = await toast.promise(
+              fetch(`${API_URL}/events/${id}`, {
+                method: "DELETE",
+                headers: {
+                  "Content-type": "application/json",
+                }
+              }),
+              {
+                pending: 'Deleting item',
+                success: 'Item Deleted',
+                error: 'error',
+              },
+              {
+                onClose: () => router.push('/account/dashboard'),
+                autoClose: 700,
+                hideProgressBar: true,
+                position: "top-center",
+              }
+          )
+        
+        if(res.ok){
+            console.log(`item id: ${id} deleted`)
+        }
+           
+          
+    }
+
     return (
         <Layout title="Dashboard">
+            <ToastContainer/>
             <h1>Dashboard</h1>
             <div>
                 {events.length==0 && <h2>No Events</h2>}
                 {events.map((e)=>(
-                    <EventItem key={e.id} event={e}/>
+                    <DashboardItem key={e.id} event={e} handleDelete={deleteItem}/>
                 ))}
             </div>
         </Layout>
